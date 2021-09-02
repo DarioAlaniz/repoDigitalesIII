21b2a5d8da2ee21af25e1268abe2f63d7417d5ce
Dario Alaniz <dario_a_066@hotmail.com.ar>
2021-09-02 00:30:11 -0300

    generacion de pwm por software, mediante systick

--- ejem_IntExt_sysTick/src/main.c
+++ ejem_IntExt_sysTick/src/main.c
@@ -21,13 +21,16 @@ void configGpio(void);
 void configIntExt(void);
 void configSysTick(void);
 
-uint8_t period = 1;
+//Variables para generacion de pwm
+uint8_t period = 50; 		//en milisegundo
+uint8_t duty   = 0;			//duty cicle
+uint8_t count  = 0;			//contador necesario para la generacion pwm
+
 uint8_t flag_Eint0 = 0;
 int main(void) {
 	configGpio();
 	configIntExt();
 	configSysTick();
-
 	while(1){
 		if (flag_Eint0) {
 			LPC_GPIO3->FIOPIN ^= (1<<LED_BLUE);
@@ -43,14 +46,14 @@ void configGpio(void){
 	LPC_GPIO0->FIODIR		|= 	(1<<PIN_OUT);				//P0.09 output
 
 	//LED
-	LPC_PINCON->PINSEL7 	&= ~(3<<18);				//P0.26
+	LPC_PINCON->PINSEL7 	&= ~(3<<18);					//P0.26
 	LPC_GPIO3->FIODIR 		|= (1<<LED_BLUE);
 
 	return ;
 }
 
 void configIntExt(void){
-	LPC_PINCON->PINSEL4 |= 	(1<<(PIN_EINT*2)); 			//P2.10 EINT0
+	LPC_PINCON->PINSEL4 |= 	(1<<(PIN_EINT*2)); 				//P2.10 EINT0
 	//pull up por defecto
 	LPC_SC->EXTMODE 	|= 1;			// interrupcion por flanco
 	LPC_SC->EXTPOLAR	&= ~1;			// flanco de bajada
@@ -67,7 +70,7 @@ void configIntExt(void){
 
 void configSysTick(void){
 	SysTick->CTRL 	|= (1<<0) | (1<<1) | (1<<2);					//	habilito el contador,
-	SysTick->LOAD  	|= ((SystemCoreClock / 1000) * period) - 1; 	// 	arranco con periodo de 1ms, max 167,7 ms
+	SysTick->LOAD  	|= ((SystemCoreClock / 1000) ) - 1; 			// 	arranco con periodo de 1ms, max 167,7 ms
 	SysTick->VAL  	=0;												//	arranca en cero
 	NVIC_SetPriority(SysTick_IRQn, 4);								// 	prioridad 4 para que sea mas importante que el EINT0
 																	// 	asi mientras espera que pase el tiempo para el anti rebote pueda interruptir
@@ -77,17 +80,21 @@ void configSysTick(void){
 
 void EINT0_IRQHandler(void){
 	flag_Eint0 = 1;
-	period+=1;
-	if(period>167) period=1;
-	for (int i = 0; i<1000000;i++); 		//retardo antes de bajar el flag asi evito efecto rebote
-	LPC_SC->EXTINT |= 1;				//limpio el flag de interrupcion
+	duty+=1;
+	if(duty>=period) duty=0;
+//	for (int i = 0; i<10000;i++); 						//retardo para evitar el anti rebore, pero se mejoro con un capacitor de 20nf
+	LPC_SC->EXTINT |= 1;								//limpio el flag de interrupcion
 }
 
-
 void SysTick_Handler(void){
-	LPC_GPIO0->FIOPIN  	^= (1<<PIN_OUT);
-	SysTick->LOAD  	     = ((SystemCoreClock / 1000) * period) - 1;
-	SysTick->VAL  		 =0;
-	SysTick->CTRL;
-	// no hace falta limpiar la bandera, pero se limpia leyendo el registro STCTRL
+	count++;
+	if(count == duty) {
+		LPC_GPIO0->FIOPIN  	&= ~(1<<PIN_OUT);			//cuando el contador es igual al duty se pone la salida en cero
+	}
+	else if (count == period){
+		LPC_GPIO0->FIOPIN  	|= (1<<PIN_OUT);			//cuando el contador llega al periodo setea la salida en 1
+		count = 0;
+	}
+	SysTick->VAL  		 	=	0;							//restablesco para que cuente desde cero cuando salga de la interrupcion
+	SysTick->CTRL;										//no hace falta limpiar la bandera, pero se limpia leyendo el registro STCTRL
 }
